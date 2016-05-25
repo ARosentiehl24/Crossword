79402e7869ebf43441f5aedb024120396f289b8b
Alberto R <alberto93-14@hotmail.com>
2016-05-25 07:23:25 -0500

    finish.

--- Crossword.py
+++ Crossword.py
@@ -1,68 +1,80 @@
-import numpy as game
+import numpy as juego
 
 
 def main():
-    words = game.array(["dog", "cat", "lion", "bear"])
-    crossword = game.array([["B", "F", "D", "O", "G"],
-                            ["C", "E", "O", "L", "L"],
-                            ["A", "F", "A", "I", "I"],
-                            ["T", "R", "G", "R", "O"],
-                            ["Z", "W", "R", "H", "N"]])
+    print("Aplicación de la Ing. Civil")
 
-    words_found = []
-    keep_playing = 1
+    palabras = juego.array(["AUTOVIAS", "PUENTES", "PRESAS", "DIQUES", "ZAPATAS", "CARRETERAS"])
+    crucigrama = juego.array([["C", "A", "R", "R", "E", "T", "E", "R", "A", "S"],
+                              ["A", "U", "O", "P", "P", "B", "U", "O", "L", "P"],
+                              ["U", "U", "O", "L", "U", "R", "U", "O", "L", "T"],
+                              ["T", "D", "G", "D", "T", "E", "E", "E", "S", "E"],
+                              ["O", "D", "I", "D", "O", "Q", "N", "S", "S", "S"],
+                              ["V", "O", "I", "Q", "V", "Q", "U", "T", "A", "A"],
+                              ["I", "O", "G", "Q", "U", "Q", "U", "E", "E", "S"],
+                              ["A", "S", "R", "D", "U", "E", "U", "E", "S", "S"],
+                              ["S", "O", "G", "D", "S", "E", "S", "E", "S", "K"],
+                              ["T", "O", "Z", "A", "P", "A", "T", "A", "S", "K"]])
 
-    while keep_playing == 1 and len(words_found) != len(words):
-        word = ""
+    palabras_encontradas = []
+    seguir_jugando = 1
 
-        print("\nWords to found:", words)
+    while seguir_jugando == 1 and len(palabras_encontradas) != len(palabras):
+        palabra = ""
 
-        print("\n", crossword, "\n")
+        print("\nPalabras a encontrar:", palabras)
 
-        initial_x = int(input("Set initial x: "))
-        initial_y = int(input("Set initial y: "))
-        final_x = int(input("Set final x: "))
-        final_y = int(input("Set final y: "))
+        print("\n", crucigrama, "\n")
 
-        print()
-
-        if initial_x == final_x:
-            for i in range(initial_y, final_y + 1, 1):
-                word += crossword[initial_x][i]
-        elif initial_y == final_y:
-            for i in range(initial_x, final_x + 1, 1):
-                word += crossword[i][initial_y]
-        elif (initial_x - final_x) == (initial_y - final_y):
-            for i in range(initial_x, final_x + 1, 1):
-                word += crossword[i][i]
-
-        if (word.lower() in words) and (word.lower() not in words_found):
-            words_found.append(word)
-
-            if initial_x == final_x and initial_y != final_y:
-                for i in range(initial_y, final_y + 1, 1):
-                    crossword[initial_x][i] = str(crossword[initial_x][i]).lower()
-            elif initial_y == final_y and initial_x != final_x:
-                for i in range(initial_x, final_x + 1, 1):
-                    crossword[i][initial_y] = str(crossword[i][initial_y]).lower()
-            elif (initial_x - final_x) == (initial_y - final_y):
-                for i in range(initial_x, final_x + 1, 1):
-                    crossword[i][i] = str(crossword[i][i]).lower()
-
-            print("Word selected", word)
-            print("You found", word, "words found", len(words_found))
-        else:
-            print("You don't found words")
+        x_inicial = int(input("Ingrese la coordenada x inicial: "))
+        y_inicial = int(input("Ingrese la coordenada y inicial: "))
+        x_final = int(input("Ingrese la coordenada x final: "))
+        y_final = int(input("Ingrese la coordenada y final: "))
 
         print()
 
-        if len(words_found) != len(words):
-            keep_playing = int(input("Do you want keep playing? (1 = yes : 0 = no): "))
+        if x_inicial == x_final:
+            for x in range(y_inicial, y_final + 1, 1):
+                palabra += crucigrama[x_inicial][x]
+        elif y_inicial == y_final:
+            for x in range(x_inicial, x_final + 1, 1):
+                palabra += crucigrama[x][y_inicial]
+        elif (x_inicial - x_final) == (y_inicial - y_final):
+            y = y_inicial
+            for x in range(x_inicial, x_final + 1, 1):
+                palabra += crucigrama[x][y]
+                y += 1
 
-            while keep_playing != 0 and keep_playing != 1:
-                keep_playing = int(input("Invalid input, do you want keep playing? (1 = yes : 0 = no): "))
+        print("Palabra seleccionada:", palabra)
+
+        if (palabra in palabras) and (palabra not in palabras_encontradas):
+            palabras_encontradas.append(palabra)
+
+            if x_inicial == x_final and y_inicial != y_final:
+                for x in range(y_inicial, y_final + 1, 1):
+                    crucigrama[x_inicial][x] = str(crucigrama[x_inicial][x]).lower()
+            elif y_inicial == y_final and x_inicial != x_final:
+                for x in range(x_inicial, x_final + 1, 1):
+                    crucigrama[x][y_inicial] = str(crucigrama[x][y_inicial]).lower()
+            elif (x_inicial - x_final) == (y_inicial - y_final):
+                y = y_inicial
+                for x in range(x_inicial, x_final + 1, 1):
+                    crucigrama[x][y] = str(crucigrama[x][y]).lower()
+                    y += 1
+
+            print("Has encontrado", palabra, "palabras encontradas", len(palabras_encontradas))
         else:
-            print("¡You win!")
+            print("No has encontrado palabras")
+
+        print()
+
+        if len(palabras_encontradas) != len(palabras):
+            seguir_jugando = int(input("¿Deseas seguir jugando? (1 = si : 0 = no): "))
+
+            while seguir_jugando != 0 and seguir_jugando != 1:
+                seguir_jugando = int(input("Entrada invalida, ¿Deseas seguir jugando? (1 = si : 0 = no): "))
+        else:
+            print("¡Has Ganado!")
 
         print("-----------------------------------------------")
 
